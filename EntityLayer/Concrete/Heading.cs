599d208393b165e52c0bcad1c4132716af01950e
MSoykan <mehmetsoykanmutlu@gmail.com>
2023-09-08 16:45:06 +0300

    Initial commit

---
+++ EntityLayer/Concrete/Heading.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete {
+    internal class Heading {
+        public int HeadingID { get; set; }
+        public string HeadingName { get; set; }
+        public DateTime HeadingDate { get; set; }
+         
+    }
+}
