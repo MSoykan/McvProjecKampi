b4508963c2c31560d38d89e8c8f2c753d9a11ac1
MSoykan <mehmetsoykanmutlu@gmail.com>
2023-09-14 16:20:52 +0300

    Ders 8-9
    -> Entitiy Layer sınıflarının değişkenleri kısıtlandırıldı. (string (300 char ) gibi.)
    -> Data Access Layer'a context sınıfı eklendi. (EntitiyLayer sınıflarını değişken olarak tutuyor.)
    -> Data Access Layer-> References -> Sağ tik-> Add reference.

--- EntityLayer/Concrete/Writer.cs
+++ EntityLayer/Concrete/Writer.cs
@@ -1,15 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EntityLayer.Concrete {
     public class Writer {
+        [Key]
         public int WriterID { get; set; }
+        [StringLength(50)]
         public int WriterName{ get; set; }
+        [StringLength(50)]
         public int WriterSurname{ get; set; }
+        public string WriterImage { get; set; }
+        [StringLength(50)]
         public int WriterMail { get; set; }
+        [StringLength(20)]
         public int WriterPassword { get; set; }
 
         public ICollection<Heading> Headings { get; set; }
